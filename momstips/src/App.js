1c0e897fc7d9775272ec3789f83d5428db2dc8ce
MarthaLizarazo <johanalizarazo_8@hotmail.com>
2019-05-11 16:28:53 -0400

    conectando firestore con react y redux

--- momstips/src/App.js
+++ momstips/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import store from './store';
+import { Provider } from 'react-redux'; // provide hace disponible a redux en toda mi aplicacion
 
 import Usuarios from './Components/Usuarios/Usuarios';
 import NuevoPost from './Components/Usuarios/NuevoPost';
@@ -9,15 +11,17 @@ import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <div className="container">
-        <Switch>
-          <Route exact path="/usuarios" component={Usuarios} />
-          <Route exact path="/usuarios/nuevo" component={NuevoPost} />
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/usuarios" component={Usuarios} />
+            <Route exact path="/usuarios/nuevo" component={NuevoPost} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
--- momstips/src/store.js
+++ momstips/src/store.js
@@ -1,15 +1,59 @@
 import { createStore, combineReducers, compose } from 'redux';
-import {
-  reactReduxFirebase,
-  firebaseReducer,
-  firestoreReducer
-} from 'react-redux-firebase';
+import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
 import { reduxFirestore, firestoreReducer } from 'redux-firestore';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
+// Boilerplate neceasario para integrar firestore en el proyecto
 // configurar firestore
 
 const firebaseConfig = {
-    
+  apiKey: 'AIzaSyC_kIAzpY9J6U1o-dvUmuxdngCwofp2DsU',
+  authDomain: 'moms-tips.firebaseapp.com',
+  databaseURL: 'https://moms-tips.firebaseio.com',
+  projectId: 'moms-tips',
+  storageBucket: 'moms-tips.appspot.com',
+  messagingSenderId: '903574016930',
+  appId: '1:903574016930:web:80c1d1af6f101121'
 };
+
+// inicializar firebase
+firebase.initializeApp(firebaseConfig);
+
+// configuracion de react-redux
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true // habilitamos para usar firestore
+};
+
+// creamos el potenciador enhacer con compose de redux y firestore
+
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig),
+  reduxFirestore(firebase)
+)(createStore);
+
+// Creando los reducers (firebase, firestore)
+
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer
+});
+
+// Creando el state inicial
+const initialState = {}; // igual a un objeto vacio, luego lo vamos llenando con firesbase
+
+// Creando store
+// compose el potenciador viene siendo una funcion que toma otras funciones
+const store = createStoreWithFirebase(
+  rootReducer,
+  initialState,
+  compose(
+    reactReduxFirebase(firebase),
+    // window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) ||
+      compose
+  )
+);
+export default store;
