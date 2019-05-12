f57fc8a3d2b7b274819730448766bf50950a6f87
MarthaLizarazo <johanalizarazo_8@hotmail.com>
2019-05-12 19:51:13 -0400

    mostrando datos en mi proyecto

--- momstips/src/Components/Usuarios/Usuarios.js
+++ momstips/src/Components/Usuarios/Usuarios.js
@@ -3,10 +3,35 @@ import { compose } from 'redux';
 import { connect } from 'react-redux'; // esta Dep. conecta un componente de react con un store de redux
 import { firestoreConnect } from 'react-redux-firebase'; // metodo para hacer las consultas
 
-const Usuarios = () => {
+const Usuarios = ({ usuario }) => {
+  if (!usuario) return <h1>Cargando...</h1>;
+
   return (
-    <div>
-      <h1>Usuario</h1>
+    <div className="row">
+      <div className="col-md-12 mb-4">{/* Agregar nuevos amigos */}</div>
+      <div className="col-md-8">
+        <h2>
+          <i className="fab fa-weixin"> </i> Chat
+        </h2>
+      </div>
+      <table className="table table-striped mt-4">
+        <thead className="text-light bg-primary">
+          <tr>
+            <th>Foto</th>
+            <th>Amigo</th>
+            <th>Comentario</th>
+          </tr>
+        </thead>
+        <tbody>
+          {usuario.map(friend => (
+            <tr key={friend.id}>
+              <td>{friend.foto}</td>
+              <td>{friend.nombre}</td>
+              <td>{friend.comentario}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
