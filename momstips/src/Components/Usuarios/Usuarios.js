266374a8ad0b5c30ff888ad1e5454543bdd0447a
MarthaLizarazo <johanalizarazo_8@hotmail.com>
2019-05-13 00:43:07 -0400

    Haciendo boton eliminar usando firestore

--- momstips/src/Components/Usuarios/Usuarios.js
+++ momstips/src/Components/Usuarios/Usuarios.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux'; // esta Dep. conecta un componente de react con un store de redux
 import { firestoreConnect } from 'react-redux-firebase'; // metodo para hacer las consultas
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
 import Spinner from '../Diseño/Spinner';
 import '../Diseño/icon.css';
 
-const Usuarios = ({ usuario }) => {
+const Usuarios = ({ usuario, firestore }) => {
   if (!usuario) return <Spinner />;
+
+  // Eliminar chat con firestore
+  const eliminarChat = id => {
+    firestore.delete({ collection: 'amigos', doc: id });
+  };
 
   return (
     <div className="row">
@@ -24,12 +32,22 @@ const Usuarios = ({ usuario }) => {
             <th>Comentario</th>
           </tr>
         </thead>
+
         <tbody>
           {usuario.map(friend => (
             <tr key={friend.id}>
               <td>{friend.foto}</td>
               <td>{friend.nombre}</td>
               <td>{friend.comentario}</td>
+              <td>
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  onClick={() => eliminarChat(friend.id)}
+                >
+                  <i className="fas fa-trash-alt" />
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
@@ -37,6 +55,12 @@ const Usuarios = ({ usuario }) => {
     </div>
   );
 };
+
+Usuarios.propTypes = {
+  firestore: PropTypes.object.isRequired,
+  usuario: PropTypes.array
+};
+
 export default compose(
   // compose aplica multiples potenciadores de store // connect va a conectar con el store
   firestoreConnect([{ collection: 'amigos' }]),
